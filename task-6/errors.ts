0183304b8bb91a27eb3330c58704f46e4fac8236
amjad <kamjad78661@gmaial.com>
2024-03-03 15:24:49 +0500

    syntax , type and assigniblity error

---
+++ task-6/errors.ts
@@ -0,0 +1,13 @@
+lett message = "Hello World";//syntax error
+console.log(message);
+
+/////
+let message = "Hello World";
+console.loger(message);  ///type error
+
+///////
+let message = "Hello World";
+message = 6;
+console.log(message); ////assignability error
+
+/////
