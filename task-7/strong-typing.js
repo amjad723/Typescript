8af0aed14d70fd8e53e56a6bb7672d00c50b574f
amjad <kamjad78661@gmaial.com>
2024-03-03 15:47:43 +0500

    strong typing

---
+++ task-7/strong-typing.js
@@ -0,0 +1,14 @@
+///strongly-typed syntax
+var a = 'pakistan';
+a = 'USA';
+var b = 19;
+var c = true;
+////
+////type infrence
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+console.log(g);
+g = true;
+console.log(g);
---
+++ task-7/strong-typing.ts
@@ -0,0 +1,15 @@
+///strongly-typed syntax
+let a:string='pakistan';
+a='USA';
+let b:number=19;
+let c:boolean=true;
+
+////
+////type infrence
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+ console.log(g)
+ g=true;
+console.log(g)
